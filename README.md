b5e078d296572f60a6c6e5474153d3498f3ccbbd
Aleksi Hartikainen <ahartik@gmail.com>
2013-11-07 01:19:30 +0100

    Cleared README, removed benchmark results
    Benchmark results used were not too meaningful, will replace later.

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
-This is my collection of wavelet tree implementations. It is work (hopefully) towards my master's thesis.
+This is my garden of wavelet trees. It is (hopefully) work towards my master's thesis.
 
 FastBitVector
 ====================
 Uncompressed bitvector with rank and select.
 - Rank is a modification Sebastiano Vigna's rank9 from 'Broadword Implementation of Rank/Select Queries.' with less space overhead.
 - Select uses its own sampling + binary search on rank-superblocks + linear search on rank-blocks.
-FastBitVector Operations
------------------------------
-- FastBitVector::rank(size_t pos, bool bit)
-  * Returns number of positions i < pos with vec[i] == bit
-- FastBitVector::select(size_t rank, bool bit)
-  * Returns smallest i with rank(i, bit) == rank
 
 SparseBitVector
 ===================
@@ -20,15 +14,6 @@ Sparse bitvector
 
 Wavelet trees
 ===========================
-Wavelet Operations
------------------
-- \*Wavelet::rank(size_t pos, intmax_t value)
-  * Returns number of positions i < pos with wt[i] == value
-  * O(log n)
-- \*Wavelet::rankLE(size_t pos, intmax_t value)
-  * Returns number of positions i < pos with wt[i] <= value
-  * O(log n)
-- Some kind of select support will be added.
 
 BalancedWavelet
 -----------------
@@ -40,12 +25,15 @@ SkewedWavelet
 - Array of different sized balanced wavelet trees.
 - Operations in O(log value)
 
+RLEWavelet<Wavelet>
+-----------------
+- Run length compressed Wavelet tree, uses SparseBitVector for run lengths.
+- rank = O(Wavelet::rank)
+- rankLE = O(Wavelet::rank + value);
+
 Benchmarks
 =================
-On my computer (i7 2600k 4.5ghz) with popcount instruction
+On my computer (i7 2600k 4.5ghz) with popcnt instruction
 - FastBitVector::rank < 30ns
 - FastBitVector::select < 40ns
-- BalancedWaveletTree::rankLE < 1000ns
-- SkewedWaveletTree::rankLE < 170ns with skewed input.
-
-See fast-bit-vector_benchmark.cpp and wavelet_benchmark.cpp.
+- See fast-bit-vector_benchmark.cpp and wavelet_benchmark.cpp.
--- wavelet_benchmark.cpp
+++ wavelet_benchmark.cpp
@@ -1,23 +1,25 @@
 #include "skewed-wavelet.h"
 #include "balanced-wavelet.h"
+#include "rle-wavelet.h"
 
 #include <iostream>
 #include <random>
 #include <chrono>
 
 using namespace std;
-void SkewedRankLE(int iters, int m) {
+
+template<typename Wt>
+void RankLE(int iters, int m, const char* name) {
   const size_t size = 1<<20;
-  const size_t max = 1<<30;
-  std::cout << "SkewedWavelet::RankLE(" << m << "):\n";
+  const size_t max = 1<<20;
+  std::cout << name << "::rankLE(" << m << "):\n";
   using namespace std::chrono;
   std::mt19937_64 mt(0);
-  std::binomial_distribution<int> gen(max, 4.0/max);
-  std::vector<int> v;
+  std::vector<uint64_t> v;
   for (size_t j = 0; j < size; ++j) {
-    v.push_back(gen(mt));
+    v.push_back(mt()%max);
   }
-  SkewedWavelet wt(v.begin(), v.end());
+  Wt wt(v.begin(), v.end());
   std::chrono::high_resolution_clock clock;
   auto start = clock.now();
   unsigned long long total = 0;
@@ -32,37 +34,16 @@ void SkewedRankLE(int iters, int m) {
   std::cout << duration_cast<nanoseconds>(end-start).count()/iters << "ns/rank\n";
 }
 
-void BalancedRankLE(int iters, int m) {
-  const size_t size = 1<<20;
-  const size_t max = 1<<30;
-  std::cout << "BalancedWavelet::RankLE(" << m << "):\n";
-  using namespace std::chrono;
-  std::mt19937_64 mt(0);
-  std::binomial_distribution<int> gen(max, 4.0/max);
-  BalancedWaveletEncoder enc(31);
-  for (size_t j = 0; j < size; ++j) {
-    enc.append(gen(mt));
-  }
-  BalancedWavelet wt(std::move(enc));
-  std::chrono::high_resolution_clock clock;
-  auto start = clock.now();
-  unsigned long long total = 0;
-  for (int j = 0; j < iters; ++j) {
-    total = total * 178923 + 987341;
-    total += wt.rankLE(total % size, m);
-  }
-  // Make sure compiler is not too smart.
-  std::cout << "(" << total << ")\n";
-  auto end = clock.now();
-  std::cout << duration_cast<nanoseconds>(end-start).count()/iters << "ns/rank\n";
-}
-
 int main() {
-  for (int i = 1; i < 16; ++i) {
-    SkewedRankLE(100000, i);
-  }
-  SkewedRankLE(100000, 1<<20);
-  for (int i = 1; i < 16; ++i) {
-    BalancedRankLE(100000, i);
-  }
+  RankLE<BalancedWavelet>(100000, 32, "BalancedWavelet");
+  RankLE<BalancedWavelet>(100000, 1<<10, "BalancedWavelet");
+  cout << endl;
+  RankLE<SkewedWavelet>(100000, 32, "SkewedWavelet");
+  RankLE<SkewedWavelet>(100000, 1<<10, "SkewedWavelet");
+  cout << endl;
+  RankLE<RLEWavelet<BalancedWavelet>>(100000, 32, "RLEWavelet<BalancedWavelet>");
+  RankLE<RLEWavelet<BalancedWavelet>>(100000, 1<<10, "RLEWavelet<BalancedWavelet>");
+  cout << endl;
+  RankLE<RLEWavelet<SkewedWavelet>>(100000, 32, "RLEWavelet<SkewedWavelet>");
+  RankLE<RLEWavelet<SkewedWavelet>>(100000, 1<<10, "RLEWavelet<SkewedWavelet>");
 }
